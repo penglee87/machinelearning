58b11438872a10702b5b50a8061c5796fa304e8b
penglee87 <penglee87@163.com>
2018-02-26 19:54:04 +0800

    20180226

--- test.py
+++ test.py
@@ -1,33 +1,64 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import linear_model
+#coding:utf-8
 
-# X is the 10x10 Hilbert matrix
-X = 1. / (np.arange(1, 11) + np.arange(0, 10)[:, np.newaxis])
-y = np.ones(10)
+# from python.Lib.packages.sklearn.tree import DecisionTreeClassifier
+# from python.Lib.packages.matplotlib.pyplot import *
+# from python.Lib.packages.sklearn.cross_validation import train_test_split
+# from python.Lib.packages.sklearn.ensemble import RandomForestClassifier
+# from python.Lib.packages.sklearn.externals.joblib import Parallel,delayed
+# from python.Lib.packages.sklearn.tree import export_graphviz
+# from python.Lib.packages.sklearn.datasets import load_iris
+# import python.Lib.packages.pandas as pd
 
-# #############################################################################
-# Compute paths
 
-n_alphas = 200
-alphas = np.logspace(-10, -2, n_alphas)
+from sklearn.tree import DecisionTreeClassifier
+from matplotlib.pyplot import *
+from sklearn.cross_validation import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.externals.joblib import Parallel,delayed
+from sklearn.tree import export_graphviz
+from sklearn.datasets import load_iris
+import pandas as pd
 
-coefs = []
-for a in alphas:
-    ridge = linear_model.Ridge(alpha=a, fit_intercept=False)
-    ridge.fit(X, y)
-    coefs.append(ridge.coef_)
+def RandomForest(dir):
+    # final = open('F:/test/final.dat' , 'r')
+    data=pd.read_csv(dir)
+    # data = [line.strip().split('\t') for line in final]
+    feature=data[[i for i in range(8)]].values
+    target=data[[8]].values
+    # target1=[target[0][i] for i in range(len(target[0]))]
+    # print feature
+    # print target
+    # feature = [[float(x) for x in row[3:]] for row in data]
+    # target = [int(row[0]) for row in data]
 
-# #############################################################################
-# Display results
+    #拆分训练集和测试集
+    # iris=load_iris()
+    #
+    # feature=iris.data
+    # target=iris.target
+    # print iris['target'].shape
+    feature_train, feature_test, target_train, target_test = train_test_split(feature, target, test_size=0.1, random_state=42)
 
-ax = plt.gca()
+    #分类型决策树
+    clf = RandomForestClassifier()
 
-ax.plot(alphas, coefs)
-ax.set_xscale('log')
-ax.set_xlim(ax.get_xlim()[::-1])  # reverse axis
-plt.xlabel('alpha')
-plt.ylabel('weights')
-plt.title('Ridge coefficients as a function of the regularization')
-plt.axis('tight')
-plt.show()
+    #训练模型
+    s = clf.fit(feature_train,target_train)
+    print (s)
+
+    #评估模型准确率
+    r = clf.score(feature_test , target_test)
+    print (r)
+
+    print (u'判定结果：%s' % clf.predict(feature_test[0]))
+    #print clf.predict_proba(feature_test[0])
+
+    print (u'所有的树:%s' % clf.estimators_)
+
+    print (clf.classes_)
+    print (clf.n_classes_)
+
+    print (u'各feature的重要性：%s' % clf.feature_importances_)
+if __name__=="__main__":
+    dir="Carseats.csv"
+    RandomForest(dir)
